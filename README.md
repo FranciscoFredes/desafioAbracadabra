a0134e50ef8a289a9cb9513c3bb9f90378818ac0
Francisco-Fredes <francisco.a.fredes@gmail.com>
2024-05-07 21:20:09 -0400

    Agregando middleware validarUsuarios, agregando rutas y funcionalidad

---
+++ README.md
@@ -0,0 +1,35 @@
+Desafío - Abracadabra
+
+Este desafío tiene como objetivo validar los conocimientos en Node.js y Express, así como la creación de rutas, manejo de objetos Request y Response, middlewares y entrega de sitios web estáticos.
+
+Descripción
+
+ En este desafío, se debe desarrollar un servidor con Express que sirva un sitio web estático con temática de magia. El objetivo del juego es encontrar un conejo oculto haciendo clic en uno de los cuatro sombreros presentados.
+
+Requerimientos
+
+   1. Creación del servidor: Se debe crear un servidor con Express en el puerto 3000.
+
+   2. Carpeta de activos estáticos: Definir la carpeta "assets" como carpeta pública del servidor.
+
+   3. Ruta de usuarios: Crear en el servidor un arreglo de nombres y devolverlo en formato JSON a través de la ruta /abracadabra/usuarios.
+
+   4. Middleware de validación de usuario: Crear un middleware con la ruta /abracadabra/juego/:usuario para validar que el usuario recibido como parámetro existe en el arreglo de nombres creado en el servidor. En caso de éxito, permitir el paso a la ruta GET correspondiente, de lo contrario, devolver la imagen "who.jpeg".
+
+   5. Ruta de conejo: Crear una ruta /abracadabra/conejo/:n que valide si el parámetro "n" coincide con el número generado de forma aleatoria. En caso de éxito, devolver la imagen del conejo, de lo contrario, devolver la imagen de Voldemort.
+
+    6. Ruta genérica: Crear una ruta genérica que devuelva un mensaje diciendo "Esta página no existe..." al consultar una ruta que no esté definida en el servidor.
+
+Uso
+
+   1.  Clona este repositorio.
+   2. Instala las dependencias usando npm install.
+   3. Ejecuta el servidor usando npm start.
+   4. Accede al sitio web estático en tu navegador.
+
+Estructura del Proyecto
+
+    index.html: Archivo HTML del sitio web estático.
+    assets/: Carpeta que contiene las imágenes y otros recursos estáticos.
+    server.js: Archivo principal que contiene la lógica del servidor Express.
+
--- server.js
+++ server.js
@@ -4,14 +4,58 @@ const app = express();
 // Definimos assets como una carpeta de archivos estaticos publicos 
 app.use(express.static("assets"));
 
+//Definimos un arreglo de usuarios para retornarlo 
+const usuarios ={ "usuarios" :
+    ["Juan", "Joselyn", "Astrid", "Maria", "Ignacia", "Javier", "Brian"]}; 
+
 // Dejamos como ruta defecto el index html
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/index.html')
     })
+
+//Ruta /abracadabra/usuarios devuelve const usuarios como un Json. 
+app.get("/abracadabra/usuarios", (req,res)=>{
+    res.json(usuarios);
+})
+
+//Middleware para validar la si el usuario existe 
+const validarUsuario = (req,res,next) => {
+  //  Convertimos a minusculas todo los datos ingresados para buscarlos dentro del arreglo
+    const usuario = req.params.usuario.toLowerCase();
+    const usuarioMinusculas = usuarios.usuarios.map(nombre => nombre.toLocaleLowerCase());
+    //Validacion de si es que el usuario se encuentra
+    if (usuarioMinusculas.includes(usuario)) {
+        next();       
+    //Respuesta en caso de no enocontrar el usuario      
+    } else {
+        res.sendFile(__dirname + '/assets/who.jpeg');
+    }
+};
+
+//Aplicando middleware en la ruta llamado a la funcion validarusuario
+app.use("/abracadabra/juego/:usuario", validarUsuario);
+
+//Ruta final del middleware 
+app.get("/abracadabra/juego/:usuario", (req, res) =>{
+    res.send("Usuario encontrado");
+})
+
+//
+app.get("/abracadabra/conejo/:n", (req, res) => {
+    const numeroAleatorio = Math.floor(Math.random() * 4) + 1; // Generar un número aleatorio entre 1 y 10
+    const n = parseInt(req.params.n);
+    if (n === numeroAleatorio) {
+        res.sendFile(__dirname + '/assets/conejito.jpg'); // Si el parámetro "n" coincide con el número aleatorio, devolver la imagen del conejo
+    } else {
+        res.sendFile(__dirname + '/assets/voldemort.jpg'); // Si no coincide, devolver la imagen de Voldemort
+    }
+});
+
     //Generando ruta de comodin al recibir cualquier url no definida
 app.get("*", (req,res)=> {
-    res.send("<center><h1>Sorry, aquí no hay nada :/ </h1></center>");
+    res.send("<center><h1>Esta página no existe</h1></center>");
 })
+
 // Definir constante PORT para entregarlo como parametro dentro de la funciona app.listen
 const PORT = 3000;
 //Iniciar el servidor en el puerto especificado
